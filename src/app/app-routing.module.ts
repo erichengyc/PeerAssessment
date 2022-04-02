353651a9da4f0d1f8959f09e588356570df18435
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-04-03 00:21:13 +0930

    created basic routes for dashboard and about

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "dashboard", component:DashboardComponent }, //create route modules
+  { path: "about", component:AboutComponent }, //create route modules
+  { path: "", redirectTo: "dashboard", pathMatch: "full" }  //set the home page as dashboard
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
