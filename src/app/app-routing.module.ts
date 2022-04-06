14f3c558841270a3f0177e700e68a1ad4b6cc2e7
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-04-06 22:20:30 +0930

    get request in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './admin/about/about.component';
 import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { ProjectsComponent } from './admin/projects/projects.component';
 
 const routes: Routes = [
   { path: "dashboard", component:DashboardComponent }, //create route modules
   { path: "about", component:AboutComponent }, //create route modules
+  { path: "projects", component:ProjectsComponent },
   { path: "", redirectTo: "dashboard", pathMatch: "full" }  //set the home page as dashboard
 ];
 
