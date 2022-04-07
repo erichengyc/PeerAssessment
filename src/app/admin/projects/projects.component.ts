3f0b55c332dc3dba83085da6d990c583a8b0d7cc
erichengyc <68725887+erichengyc@users.noreply.github.com>
2022-04-07 22:50:31 +0930

    delete in angular

--- src/app/admin/projects/projects.component.ts
+++ src/app/admin/projects/projects.component.ts
@@ -13,6 +13,8 @@ export class ProjectsComponent implements OnInit
   newProject: Project = new Project();
   editProject: Project = new Project();
   editIndex: any = null;
+  deleteProject: Project = new Project();
+  deleteIndex: any=null;
 
   constructor(private projectsService: ProjectsService)
   {
@@ -78,4 +80,30 @@ export class ProjectsComponent implements OnInit
 
     });
   }
+
+  onDeleteClick(event: any, index: number)
+  {
+    this.deleteIndex = index;
+    this.deleteProject.projectID = this.projects[index].projectID;
+    this.deleteProject.projectName = this.projects[index].projectName;
+    this.deleteProject.dateOfStart = this.projects[index].dateOfStart;
+    this.deleteProject.teamSize = this.projects[index].teamSize;
+  }
+
+  onDeleteConfirmClick()
+  {
+    this.projectsService.deleteProject(this.deleteProject.projectID).subscribe(
+      (response) => {
+        this.projects.splice(this.deleteIndex, 1);
+        this.deleteProject.projectID = null;
+        this.deleteProject.projectName = null;
+        this.deleteProject.dateOfStart = null;
+        this.deleteProject.teamSize = null;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
 }
